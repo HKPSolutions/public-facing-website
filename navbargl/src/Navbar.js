d1982673cf194ca5edbef5a97f9305c2fbe5358e
georgeliASC20 <68346402+georgeliASC20@users.noreply.github.com>
2020-10-13 01:04:27 -0400

    Adding my attempt of the Navbar

---
+++ navbargl/src/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';//put some styling in APP.css
+//this created the hyperlinks.
+function Navbar(){
+    return(
+
+    <div className= "Hello">
+        
+        
+        
+        <a href="#news">App</a>  
+        <a href="#contact">Demo</a>
+        <a href="#contact">Contact</a>
+
+    
+
+    </div>
+
+    );
+
+
+
+
+}
+
+export default Navbar;
