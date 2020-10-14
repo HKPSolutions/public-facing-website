eb64d4e8410486fcef0786f7bda39bbdfe14f434
johnjamespj <johnjamespj@gmail.com>
2020-10-14 16:57:40 -0400

    implemented style and layout
    * style layout
    * better naming for element classes

--- app/src/components/Footer/Footer.js
+++ app/src/components/Footer/Footer.js
@@ -5,14 +5,14 @@ import Branding from './assets/branding.png'
 
 function Footer() {
   return (
-    <div className="footer__container">
+    <div className="footer">
       <div className="footer__branding">
         <a href="!#"><img src={Branding} alt="brand icon" /></a>
       </div>
       <div className="footer__info">
-        Improving hotel housekeeping by paying housekeepers by the quantity and quality of rooms cleaned
+        <p>Improving hotel housekeeping by paying housekeepers by the quantity and quality of rooms cleaned</p>
       </div>
-      <div className="footer__nav">
+      <div className="footer__nav footer__lists">
         <h2>Menu</h2>
         <ul>
           <li><a href="!#">Home</a></li>
@@ -21,13 +21,13 @@ function Footer() {
           <li><a href="!#">Contact</a></li>
         </ul>
       </div>
-      <div className="foooter__contact-info">
+      <div className="foooter__contact-info footer__lists">
         <h2>Contact</h2>
         <ul>
           <li>Email: <a href="mailto:hkpsolutions@gmail.com">hkpsolutions@gmail.com</a></li>
         </ul>
       </div>
-      <div className="footer__social">
+      <div className="footer__social footer__lists">
         <h2>follow</h2>
         <ul>
           <li><a href="!#">LinkedIn</a></li>
