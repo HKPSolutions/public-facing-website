9fbb258db27b61fac3aa704f9989486d30c8e5ff
johnjamespj <johnjamespj@gmail.com>
2020-10-15 14:52:28 -0400

    copied files to respective folders

--- app/src/components/Pages/HomePage/HomePage.js
+++ app/src/components/Pages/HomePage/HomePage.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import './HomePage.scss';
+import React, { Component } from 'react';
+import IntroSection from "./components/IntroSection";
+import FeaturesPitch from "./components/FeaturesPitch";
+import AnalyticsPitch from "./components/AnalyticsPitch";
+import PaymentPitch from "./components/PaymentPitch";
 
-function App() {
-  return (
-    <div className="App">
-        <p>
-          HKP Solutions
-        </p>
-    </div>
-  );
+class HomeView extends Component {
+    render() {
+        return (
+            <div>
+                <IntroSection />
+                <FeaturesPitch />
+                <AnalyticsPitch />
+                <PaymentPitch />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default HomeView;
---
+++ app/src/components/Pages/HomePage/components/IntroSection.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function IntroSection(props) {
+    return (
+        <div class="intro-container">
+            <div class="intro vertical-center">
+                <div class="intro-header">
+                    Revolutionizing the Way Your Housekeepers are Paid.
+                </div>
+                <div class="intro-desc">
+                    Improving hotel housekeeping by paying housekeepers by the quantity and quality of rooms cleaned
+                </div>
+                <div class="intro-buttons">
+                    <button class="intro-button">Sign Up</button>
+                    <button class="intro-button">Learn More</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default IntroSection;
