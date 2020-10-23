94eb58bc4016e9391cc563a6aac105d3777281bf
Caleb Arulandu <calebarulandu@gmail.com>
2020-10-23 15:08:47 -0400

    fix 5

--- app/src/components/App/App.js
+++ app/src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Switch, Route, HashRouter } from 'react-router-dom'
 import HomePage from '../Pages/HomePage/HomePage'
 import DemoPage from '../Pages/DemoPage/DemoPage'
 import ContactPage from '../Pages/ContactPage/ContactPage'
@@ -11,18 +11,18 @@ import Footer from '../Footer/Footer'
 
 function App() {
   return (
-    <BrowserRouter history={hashHistory} basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <NavBar />
       <div className="App">
         <Switch>
-          <Route path='/' component={HomePage} />
-          <Route path='/demo' component={DemoPage} />
-          <Route path='/contact' component={ContactPage} />
-          <Route path='/app' component={AppPage} />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/demo" component={DemoPage} />
+          <Route path="/contact" component={ContactPage} />
+          <Route path="/app" component={AppPage} />
         </Switch>
       </div>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
